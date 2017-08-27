d652edcf997b781c0eb2de8d47307dd69da520ca
kv9991 <exide-pra@yandex.ru>
2017-08-28 00:37:11 +0300

    white list

--- white.js
+++ white.js
@@ -1,12 +1,23 @@
 module.exports = {
 	hosts: [
-		'http://promo.levelupworlds.com', 
-		'https://promo.levelupworlds.com', 
+		'http://smm.levelupworlds.com', 
+		'https://smm.levelupworlds.com', 
 		'http://web.levelupworlds.com', 
 		'https://web.levelupworlds.com', 
-		'http://185.22.232.114'
+		'http://film.levelupworlds.com', 
+		'https://film.levelupworlds.com', 
+		'http://music.levelupworlds.com', 
+		'https://music.levelupworlds.com', 
+		'http://levelupworlds.com', 
+		'https://levelupworlds.com',
+		'http://levelup.name', 
+		'https://levelup.name'
 	],
 	applications: [
-		'03258934'
+		'03258934',
+		'53993485',
+		'78348369',
+		'73285761',
+		'16462384'
 	]
 }
